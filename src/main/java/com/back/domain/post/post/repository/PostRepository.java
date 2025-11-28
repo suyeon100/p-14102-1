eebaeddef25f8c03fc61edf0c7be94d5cd0a8eee
suyeon100 <xxyx9811@gmail.com>
2025-11-28 11:36:14 +0900

    work

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -1,8 +1,7 @@
 package com.back.domain.post.post.repository;
 
-public class PostRepository {
+import com.back.domain.post.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
-
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
